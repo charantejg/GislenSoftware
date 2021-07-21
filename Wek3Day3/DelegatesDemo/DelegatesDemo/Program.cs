e7114f8b841d1c8a1744aa7f6eabf6b9749bc47e
ctej <charan.tej@aspect.com>
2021-07-21 12:43:15 +0530

    week3 Day 3 Delegates , Generics

---
+++ Wek3Day3/DelegatesDemo/DelegatesDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace DelegatesDemo
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+           var billingSystem = new BillingSystem();
+           var customer1 = new NewCustomer();
+           var customer2 = new ExistingCustomer();
+
+           billingSystem.CalculateDiscount(customer1.Discount);
+           billingSystem.CalculateDiscount(customer2.Discount);
+
+
+
+           Action<int> printAction = Print;
+           Func<int, int> displayAction = Display;
+           var result = displayAction(10);
+
+        }
+
+        static void Print(int val)
+        {
+            Console.WriteLine(val * 10);
+        }
+
+        static int Display(int val)
+        {
+            return val * 10;
+        }
+
+    }
+
+
+    class BillingSystem
+    {
+        public delegate void DiscountHandler();
+
+        public void PrintBill()
+        {
+            Console.WriteLine("The bill amount");
+        }
+         
+        public void CalculateDiscount(DiscountHandler handler)
+        {
+
+            handler();
+        }
+        
+    }
+
+   
+
+    class NewCustomer
+    {
+        public void Discount()
+        {
+            Console.WriteLine("The discount is 10%");
+        }
+
+    }
+
+    class  ExistingCustomer
+    {
+        public void Discount()
+        {
+            Console.WriteLine("The discount is 20%");
+        }
+    }
+}
---
+++ Wek3Day3/GenericsDelegateEvent/GenericsDelegateEvent/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GenericsDelegateEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //  Generic classes
+            var bookList = new BookList();
+            bookList.Add(new Book());
+
+            int i = 10;
+            float f = 5.1f;
+            var genericList = new GenericList<Book>();
+            genericList.Add(new Book());
+            genericList.GetPrice(new Book(), i);
+            genericList.GetPrice(new Book(), f);
+            genericList.GetDiscount(10);
+            // genericList.Add(new EBook());
+           
+
+
+            // DELEGATES 
+            var photo = new Photo() {Path = "c:\\phtoto\\1.jpj"};
+            var photoProcessor = new PhotoProcessor();
+           // photoProcessor.Process(photo);
+            var filters = new PhotoFilter();
+
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+
+            photoProcessor.ProcessNew(photo, filterHandler);
+           
+            //Simple example 
+            MyDisplayDelegate delegateToDisplay = Display;
+            delegateToDisplay = DisplayToBiggerScreen;
+
+            delegateToDisplay("Testing");
+
+   
+        }
+
+        public delegate void MyDisplayDelegate(string message);
+
+        static void Display(string message)
+        {
+            Console.WriteLine("This is from small sreen");
+        }
+
+        static void DisplayToBiggerScreen(string message)
+        {
+            Console.WriteLine("This is from big Screen");
+        }
+
+
+    }
+
+    class PhotoProcessor
+    {
+        //public void Process(Photo photo)
+        //{
+        //    var filters = new PhotoFilter();
+        //    filters.ApplyBrightness(photo);
+        //    filters.ApplyContrast(photo);
+            
+        //}
+
+        public void ProcessNew(Photo photo, PhotoFilterHandler filterHandler)
+        {
+            filterHandler(photo);
+        }
+
+       public delegate void PhotoFilterHandler(Photo photo);
+
+    }
+
+    class PhotoFilter
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Applying the Brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Applying the Contrast");
+        }
+
+        // Exposure - 
+    }
+
+    public class Photo
+    {
+        public string Path { get; set; }
+    }
+
+
+    class GenericList<T>
+    {
+       
+        public int price { get; set; }
+        public void Add(T myObj)
+        {
+            //
+        }
+
+        public double GetPrice<T1>(T myobj, T1 tax)
+        {
+            return 0;
+        }
+
+        public void GetDiscount(int discount)
+        {
+
+        }
+
+    }
+
+
+    class BookList
+    {
+        public void Add(Book book)
+        {
+            // adding logic
+        }
+
+        public double GetPrice(Book book, int tax)
+        {
+            return 0;
+        }
+
+    }
+
+    class Book
+    {
+        public int Isbn { get; set; }
+        public string Title { get; set; }
+    }
+
+    class EBook
+    {
+        public string Url { get; set; }
+    }
+
+}
